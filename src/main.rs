6418555afcf44dfdc5edb74552007ccd1db9f8ec
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2023-04-25 04:40:55 +0900

    add: Section 2.2, "Bounds checking and pointers"

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,23 @@ fn check_estimate_size() {
     assert!(y < 10);
 }
 
+fn get_wrapped(i: usize, a: &[u32]) -> u32 {
+    if a.len() == 0 {
+        return 0;
+    }
+    return a[(i % a.len() + 1) % a.len()];
+}
+
+#[cfg(kani)]
+#[kani::proof]
+fn bound_check() {
+    let size: usize = kani::any();
+    kani::assume(size < 4096);
+    let index: usize = kani::any();
+    let array: Vec<u32> = vec![0; size];
+    get_wrapped(index, &array);
+}
+
 fn main() {
     println!("Hello, world!");
 }
